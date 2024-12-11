78b2bdce0883048cd5b97125288e20e09d984370
GiuseppeArbore <s329235@studenti.polito.it>
2024-12-11 03:15:42 +0100

    add camera

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,61 +4,96 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { useState } from 'react';
 import { Button, Image, StyleSheet, Platform, View } from 'react-native';
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { Text, TouchableOpacity } from 'react-native';
 
 
 export default function HomeScreen() {
-  
+  const [facing, setFacing] = useState<CameraType>('back');
+  const [permission, requestPermission] = useCameraPermissions();
+
+
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
   return (
+    <View style={styles.container}>
+      <CameraView style={styles.camera} facing={facing}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
+    </View>
 
-        <ParallaxScrollView
-          headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-          headerImage={
-            <Image
-              source={require('@/assets/images/partial-react-logo.png')}
-              style={styles.reactLogo}
-            />
-          }>
-          <ThemedView style={styles.titleContainer}>
-            <ThemedText type="title">Meow</ThemedText>
-            <HelloWave />
-          </ThemedView>
-          <ThemedView style={styles.stepContainer}>
-            <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-            <ThemedText>
-              Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-              Press{' '}
-              <ThemedText type="defaultSemiBold">
-                {Platform.select({
-                  ios: 'cmd + d',
-                  android: 'cmd + m',
-                  web: 'F12'
-                })}
-              </ThemedText>{' '}
-              to open developer tools.
-            </ThemedText>
-          </ThemedView>
-          <ThemedView style={styles.stepContainer}>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-            <ThemedText>
-              Tap the Explore tab to learn more about what's included in this starter app.
-            </ThemedText>
-          </ThemedView>
-          <ThemedView style={styles.stepContainer}>
-            <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-            <ThemedText>
-              When you're ready, run{' '}
-              <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-              <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-              <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-              <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-            </ThemedText>
-          </ThemedView>
-          <Button title="Open Camera"  />
-        </ParallaxScrollView>
-
+    /*<ParallaxScrollView
+       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+       headerImage={
+         <Image
+           source={require('@/assets/images/partial-react-logo.png')}
+           style={styles.reactLogo}
+         />
+       }>
+       <ThemedView style={styles.titleContainer}>
+         <ThemedText type="title">Meow</ThemedText>
+         <HelloWave />
+       </ThemedView>
+       <ThemedView style={styles.stepContainer}>
+         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+         <ThemedText>
+           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+           Press{' '}
+           <ThemedText type="defaultSemiBold">
+             {Platform.select({
+               ios: 'cmd + d',
+               android: 'cmd + m',
+               web: 'F12'
+             })}
+           </ThemedText>{' '}
+           to open developer tools.
+         </ThemedText>
+       </ThemedView>
+       <ThemedView style={styles.stepContainer}>
+         <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+         <ThemedText>
+           Tap the Explore tab to learn more about what's included in this starter app.
+         </ThemedText>
+       </ThemedView>
+       <ThemedView style={styles.stepContainer}>
+         <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
+         <ThemedText>
+           When you're ready, run{' '}
+           <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
+           <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
+           <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
+           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+         </ThemedText>
+       </ThemedView>
+       <Button title="Open Camera"  />
+     </ParallaxScrollView>
+*/
   );
 }
 
+/*
 const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
@@ -75,5 +110,35 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     position: 'absolute',
+  },
+});
+*/
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
