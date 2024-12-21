09330e9d52faa223a5580242a0b9ad17b1da9520
Claudia Maggiulli <s332252@studenti.polito.it>
2024-12-21 11:29:54 +0100

    login_form added

--- app/(tabs)/maps.tsx
+++ app/(tabs)/maps.tsx
@@ -1,31 +1,47 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+    StyleSheet,
+    View,
+    Modal,
+    TextInput,
+    Text,
+    TouchableOpacity,
+    useColorScheme
+} from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { Linking } from 'react-native';
+import { Login_form } from '@/components/Login_form';
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    map: {
-        ...StyleSheet.absoluteFillObject,
-    },
-});
 
 const TabThreeScreen: React.FC = () => {
+
+    const colorScheme = useColorScheme();
+    const isDarkMode = colorScheme === 'dark';
+
 
     const openGoogleMaps = (latitude: number, longitude: number) => {
         const url = `https://www.google.com/maps/dir/?api=1&destination=${latitude},${longitude}`;
         Linking.openURL(url).catch(err => console.error('Error:', err));
     };
 
+    const styles = StyleSheet.create({
+            container: {
+                flex: 1,
+            },
+            map: {
+                ...StyleSheet.absoluteFillObject,
+            },
+        });
+
+
     return (
         <View style={styles.container}>
+            <Login_form />
             <MapView
                 style={styles.map}
                 initialRegion={{
-                    latitude: 37.7749, // Initial latitude
-                    longitude: -122.4194, // Initial longitude
+                    latitude: 37.7749,
+                    longitude: -122.4194,
                     latitudeDelta: 0.0922,
                     longitudeDelta: 0.0421,
                 }}
@@ -42,5 +58,3 @@ const TabThreeScreen: React.FC = () => {
 };
 
 export default TabThreeScreen;
-
-
